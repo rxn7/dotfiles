70e2ed5d607729c6b385aad38b000a546c2765b4
rxn <rotthin_dev@protonmail.com>
2022-04-11 22:28:32 +0200

    Set pl keyboard layout on autostart

--- sh/autostart.sh
+++ sh/autostart.sh
@@ -2,6 +2,7 @@ xrandr --output DP-1 --rate 144 --mode 1920x1080 --right-of HDMI-1
 xrandr --output HDMI-1 --rate 144 --mode 1920x1080
 
 xset r rate 120 30
+setxkbmap pl
 
 killall dwmblocks ; dwmblocks &
 killall volumeicon ; volumeicon &
@@ -9,3 +10,4 @@ killall dunst ; dunst &
 killall xrootgif ; xrootgif /home/rxn/pics/wallpapers/lake.gif &
 killall xcompmgr ; xcompmgr &
 killall volumeicon ; volumeicon &
+killall polkit-dumb-agent ; polkit-dumb-agent &
