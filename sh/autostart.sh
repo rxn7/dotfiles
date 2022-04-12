3d84d87eee22a4f2ab0c778a16266941ecd103c6
rxn <rotthin_dev@protonmail.com>
2022-04-13 01:36:17 +0200

    Much better autostart script

--- sh/autostart.sh
+++ sh/autostart.sh
@@ -1,13 +1,30 @@
+#!/bin/bash
+
+declare -a programs
+programs=(
+	"dwmblocks"
+	"volumeicon"
+	"dunst"
+	"xrootgif /home/rxn/pics/wallpapers/lake.gif"
+	"xcompmgr"
+	"volumeicon"
+	"polkit-dumb-agent"
+);
+
+# Settings
 xrandr --output DP-1 --rate 144 --mode 1920x1080 --right-of HDMI-1
 xrandr --output HDMI-1 --rate 144 --mode 1920x1080
-
 xset r rate 120 30
 setxkbmap pl
 
-killall dwmblocks ; dwmblocks &
-killall volumeicon ; volumeicon &
-killall dunst ; dunst &
-killall xrootgif ; xrootgif /home/rxn/pics/wallpapers/lake.gif &
-killall xcompmgr ; xcompmgr &
-killall volumeicon ; volumeicon &
-killall polkit-dumb-agent ; polkit-dumb-agent &
+restart() {
+	echo "Restarting $1"
+	pkill $1
+	cmd=$*
+	$cmd &>/dev/null & disown;
+}
+
+for prog in "${programs[@]}"
+do
+	restart $prog
+done
