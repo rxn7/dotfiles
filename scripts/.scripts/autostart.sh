959a8c225662819bd2123c0ea43a9486586b0514
rxtthin <rotthin_dev@protonmail.com>
2022-11-18 11:24:33 +0100

    Corrected xsetkbmap to setxkbmap

--- scripts/.scripts/autostart.sh
+++ scripts/.scripts/autostart.sh
@@ -4,7 +4,7 @@ restart() {
     [ -z "$(pidof -x $1)" ] && ${2:-$1} &
 }
 
-xsetkbmap pl
+setxkbmap pl
 xrandr --output DisplayPort-0 --primary --mode 1920x1080 --pos 1920x590 --rotate normal --rate 144 --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate normal --rate 144
 xset r rate 300 70
 restart volumeicon
