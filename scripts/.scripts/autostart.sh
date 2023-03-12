e6586cff171c604c4d8c0ea29c4eb79b7976df49
rxn7 <rotthin_dev@protonmail.com>
2023-03-12 02:13:57 +0100

    Flipped monitor

--- scripts/.scripts/autostart.sh
+++ scripts/.scripts/autostart.sh
@@ -5,7 +5,7 @@ restart() {
 }
 
 setxkbmap pl
-xrandr --output DisplayPort-0 --primary --mode 1920x1080 --pos 1920x590 --rotate normal --rate 144 --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate normal --rate 120
+xrandr --output DisplayPort-0 --primary --mode 1920x1080 --pos 1080x531 --rotate normal --rate 144 --output DisplayPort-1 --off --output DisplayPort-2 --off --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate left --rate 120
 xset r rate 300 70
 
 restart volumeicon
